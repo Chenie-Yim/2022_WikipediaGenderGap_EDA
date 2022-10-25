cae2eb852e7c3b7e73f1aeb4246fad89bf296a39
Chaeyeon Yim <{chaeyeon.yim95@gmail.com}>
2022-10-24 22:14:09 -0400

    upadate requirements

--- mid.py
+++ mid.py
@@ -1,6 +1,7 @@
 from turtle import color
 from xml.dom import WRONG_DOCUMENT_ERR
 import pandas as pd
+import numpy as np
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
